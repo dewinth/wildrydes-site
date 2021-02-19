dd7007c1b12a876e5888c0954b2cc9a5e11bca30
dewinth <40359007+dewinth@users.noreply.github.com>
2021-02-19 13:47:20 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_Z4sI4ZBsm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1b35udm52aa4s0lf45ltb58rfr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
